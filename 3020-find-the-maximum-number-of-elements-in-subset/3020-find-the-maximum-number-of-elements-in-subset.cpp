8f30904edd8e56118f2aa39442f06c64ab17dad4
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-06-27 10:47:02 +0530

    Time: 568 ms (5.14%), Space: 129.9 MB (13.14%) - LeetHub

---
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    int maximumLength(vector<int>& nums) {
+
+        unordered_map<int,int>freq;
+        for (auto num:nums){
+            freq[num]++;
+        }
+
+        vector<int>new_nums;
+        for (auto [num,count]:freq){
+            if (num==1) continue;
+            new_nums.push_back(num);
+        }
+        sort(new_nums.begin(),new_nums.end());
+
+        int max_sz=0;
+        int n=new_nums.size();
+        for (int i=n-1;i>=0;i--){
+            cout<<new_nums[i]<<" ";
+            int len=1;
+            int num=new_nums[i];
+            int prev=sqrt(num);
+            while( (prev*prev)==num && freq[prev]>1){
+                len+=2;
+                num=prev;
+                prev=sqrt(num);
+            }
+            max_sz=max(max_sz,len);
+        }
+        if (freq.find(1)!=freq.end()){
+            if (freq[1]%2==0) freq[1]-=1;
+            max_sz=max(max_sz,freq[1]);
+        }
+        return max_sz;
+    }
+};
