e0bbea40bc6c80c7260634bcd7cb1bd9a09a7006
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-07-01 12:41:45 +0530

    Time: 2107 ms (5.11%), Space: 543.5 MB (5.26%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>>dir={{-1,0},{1,0},{0,-1},{0,1}};
+        vector<vector<bool>>vis(m,vector<bool>(n,false));
+        vector<vector<long long>>moves(m,vector<long long>(n,1e9));
+        priority_queue<vector<long long>,vector<vector<long long>>,greater<vector<long long>>>pq;
+        pq.push({0,0,0});
+        moves[0][0]=0;
+
+        while(!pq.empty()){
+            auto vec=pq.top();
+            pq.pop();
+            int move=vec[0];
+            int r=vec[1];
+            int c=vec[2];
+
+            if (r==m-1 && c==n-1) return move;
+
+            for (auto d:dir){
+                int nr=r+d[0];
+                int nc=c+d[1];
+                if (nr>=0 && nr<m && nc>=0 && nc<n){
+                    if (moves[nr][nc]>move+grid[nr][nc]){
+                        moves[nr][nc]=move+grid[nr][nc];
+                        pq.push({moves[nr][nc],nr,nc});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
