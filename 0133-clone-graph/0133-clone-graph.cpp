274e1c19d636d94354aceec5c0d37d1963ee4f9d
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-07-03 12:41:45 +0530

    Time: 3 ms (68.35%), Space: 11.8 MB (90.64%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -22,7 +22,9 @@ public:
 class Solution {
 public:
     void dfs(Node* clone_node, Node* node, map<Node*,Node*>&mpp){
+
         for (Node*adjnode:node->neighbors){
+
             if (mpp.find(adjnode)==mpp.end()){
                 Node* clone=new Node(adjnode->val);
                 mpp[adjnode]=clone;
@@ -41,8 +43,31 @@ public:
         Node* clone_node=new Node(node->val);
         mpp[node]=clone_node;
 
-        dfs(clone_node,node,mpp);
-        return clone_node;
+        // dfs(clone_node,node,mpp);
+        // return clone_node;
 
+        // solve using bfs
+
+        queue<Node*>q;
+        q.push(node);
+        while(!q.empty()){
+            Node* ori_node=q.front();
+            Node* clone_node=mpp[ori_node];
+            q.pop();
+            for (Node* adjnode: ori_node->neighbors){
+
+                if (mpp.find(adjnode)==mpp.end()){
+                    Node* clone = new Node(adjnode->val);
+                    mpp[adjnode]=clone;
+                    clone_node->neighbors.push_back(clone);
+                    q.push(adjnode);
+                }
+                else{
+                    clone_node->neighbors.push_back(mpp[adjnode]);
+                }
+            }
+        }
+
+        return clone_node;
     }
 };
