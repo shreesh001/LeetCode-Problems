e5e419d354fb6678413d55f5fb480f6e57f615eb
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-07-15 02:13:50 +0530

    Time: 2277 ms (5.21%), Space: 328 MB (25.22%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -17,7 +17,41 @@ public:
         return dp[ind][seq1][seq2]=res%mod;
     }
     int subsequencePairCount(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0,0,0)-1;
+        // memset(dp,-1,sizeof(dp));
+        // return solve(nums,0,0,0)-1;
+
+
+        // solving using bottom up dp
+        
+        int n=nums.size();
+        int maxval=0;
+        for (int i=0;i<n;i++){
+            maxval=max(maxval,nums[i]);
+        }
+
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(maxval+1,vector<int>(maxval+1,0)));
+
+        for (int seq1=0;seq1<=maxval;seq1++){
+            for (int seq2=0;seq2<=maxval;seq2++){
+                if (seq1!=0 && seq2!=0 && seq1==seq2){
+                    dp[n][seq1][seq2]=1;
+                }
+            }
+        }
+
+        for (int i=n-1;i>=0;i--){
+            for (int seq1=maxval;seq1>=0;seq1--){
+                for (int seq2=maxval;seq2>=0;seq2--){
+
+                    long long skip=dp[i+1][seq1][seq2];
+                    long long take1=dp[i+1][gcd(seq1,nums[i])][seq2];
+                    long long take2=dp[i+1][seq1][gcd(seq2,nums[i])];
+
+                    dp[i][seq1][seq2]=(skip+take1+take2)%mod;
+                }
+            }
+        }
+
+        return dp[0][0][0];
     }
 };
