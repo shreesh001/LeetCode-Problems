fa9a37acb02dd42444e876108be678026c77e462
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-07-18 10:46:51 +0530

    Time: 110 ms (27.71%), Space: 123.2 MB (74.7%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
     vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
-        int m = *max_element(nums.begin(), nums.end());
-        vector<long long> cnt(m + 1);
-        for (int num : nums) {
-            cnt[num]++;
-        }
-        for (int i = 1; i <= m; i++) {
-            for (int j = i * 2; j <= m; j += i) {
-                cnt[i] += cnt[j];
+        int n=nums.size();
+        int maxi=*max_element(nums.begin(),nums.end());
+
+        vector<long long>factors(maxi+1,0);
+
+        // finding freq of factors
+        for (int j=0;j<n;j++){
+            int num=nums[j];
+            for (int i=1;i*i<=num;i++){
+                if (num%i==0){
+                    factors[i]++;
+                    if ((num/i)!=i) factors[num/i]++;
+                }
             }
         }
-        for (int i = 1; i <= m; i++) {
-            cnt[i] = cnt[i] * (cnt[i] - 1) / 2;
+
+        // finding the pairs
+        for (int i=1;i<=maxi;i++){
+            long long freq=factors[i];
+            if (freq<2){
+                factors[i]=0;
+                continue;
+            }
+            factors[i]=(freq*(freq-1))/2;
         }
-        for (int i = m; i >= 1; i--) {
-            for (int j = i * 2; j <= m; j += i) {
-                cnt[i] -= cnt[j];
+
+        //fixing the cnt of pairs
+
+        for (int i=maxi;i>=1;i--){
+            for (int j=2*i;j<=maxi;j+=i){
+                factors[i]-=factors[j];
             }
         }
-        for (int i = 1; i <= m; i++) {
-            cnt[i] += cnt[i - 1];
+
+        // converting it to prefix sum so that i can find the value at the particular index
+        for (int i=1;i<=maxi;i++){
+            factors[i]+=factors[i-1];
         }
-        vector<int> ans;
-        for (long long q : queries) {
-            q++;
-            int pos = lower_bound(cnt.begin(), cnt.end(), q) - cnt.begin();
-            ans.push_back(pos);
+        
+        vector<int>ans;
+        for (auto q:queries){
+            auto ind=upper_bound(factors.begin(),factors.end(),q)-factors.begin();
+            ans.push_back(ind);
         }
         return ans;
     }
