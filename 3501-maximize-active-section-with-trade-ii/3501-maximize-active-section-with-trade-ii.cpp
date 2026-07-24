f63b0d24c7a15ad232152d8a4b5a1f134e4d7ea4
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-07-24 15:59:13 +0530

    Time: 371 ms (8.78%), Space: 293.3 MB (7.82%) - LeetHub

--- 3501-maximize-active-section-with-trade-ii/3501-maximize-active-section-with-trade-ii.cpp
+++ 3501-maximize-active-section-with-trade-ii/3501-maximize-active-section-with-trade-ii.cpp
@@ -1,107 +1,93 @@
 class Solution {
 public:
+    vector<int>seg;
+    void buildsegtree(int ind,int l,int r,vector<int>&pair_sum){
+        if (l==r){
+            seg[ind]=pair_sum[l];
+            return;
+        }
+        int mid=(l+r)/2;
+        buildsegtree(2*ind+1,l,mid,pair_sum);
+        buildsegtree(2*ind+2,mid+1,r,pair_sum);
+        seg[ind]=max(seg[2*ind+1],seg[2*ind+2]);
+        return;
+    }
+
+    int Range_Maximum(int ind,int s,int e,int l,int r,vector<int>&pair_sum){
+        if (e<l || r<s) return INT_MIN;
+        if (s<=l && r<=e) return seg[ind];
+
+        int mid=(l+r)/2;
+
+        return max(Range_Maximum(2*ind+1,s,e,l,mid,pair_sum),Range_Maximum(2*ind+2,s,e,mid+1,r,pair_sum));
+    }
+
     vector<int> maxActiveSectionsAfterTrade(string s, vector<vector<int>>& queries) {
-        int n = s.length();
-        int total_ones = 0;
-        for (char c : s) {
-            if (c == '1') total_ones++;
+        
+        int n=s.size();
+        int total_one=0;
+        for (auto ch:s){
+            if (ch=='1') total_one++;
         }
-        
-        vector<int> type;
-        vector<int> start;
-        vector<int> end_idx;
-        
-        for (int i = 0; i < n; ) {
-            int j = i;
-            while (j < n && s[j] == s[i]) {
-                j++;
+
+        vector<int>startInd;
+        vector<int>endInd;
+        vector<int>size;
+        for (int i=0;i<n;){
+            if (s[i]=='0'){
+                startInd.push_back(i);
+                while(i<n && s[i]=='0'){
+                    i++;
+                }
+                endInd.push_back(i-1);
+                size.push_back(endInd.back()-startInd.back()+1);
             }
-            type.push_back(s[i] - '0');
-            start.push_back(i);
-            end_idx.push_back(j - 1);
-            i = j;
+            i++;
         }
-        
-        int N = type.size();
-        
-        vector<int> pos_to_seg(n);
-        for (int i = 0; i < N; i++) {
-            for (int j = start[i]; j <= end_idx[i]; j++) {
-                pos_to_seg[j] = i; 
-            }
+        int m=size.size();
+        if (m<2){
+            return vector<int>(queries.size(),total_one);
         }
-        
-        vector<int> ans(N, 0);
-        for (int i = 1; i < N - 1; i++) {
-            if (type[i] == 1) {
-                ans[i] = (end_idx[i - 1] - start[i - 1] + 1) + (end_idx[i + 1] - start[i + 1] + 1);
-            }
+
+        vector<int>pair_sum;
+        for (int i=1;i<m;i++){
+            pair_sum.push_back(size[i]+size[i-1]);
         }
-        
-        vector<int> log_table(N + 1, 0);
-        for (int i = 2; i <= N; i++) {
-            log_table[i] = log_table[i / 2] + 1;
-        }
-        
-        int K = log_table[N] + 1;
-        vector<vector<int>> st(K, vector<int>(N, 0));
-        
-        for (int i = 0; i < N; i++) {
-            st[0][i] = ans[i];
-        }
-        
-        for (int j = 1; j < K; j++) {
-            for (int i = 0; i + (1 << j) <= N; i++) {
-                st[j][i] = max(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
-            }
-        }
-        
-        auto queryRMQ = [&](int L_idx, int R_idx) {
-            if (L_idx > R_idx) return 0;
-            int j = log_table[R_idx - L_idx + 1];
-            return max(st[j][L_idx], st[j][R_idx - (1 << j) + 1]);
-        };
-        
-        auto eval = [&](int i, int L, int R, int segL, int segR) {
-            if (i <= segL || i >= segR) return 0;
-            if (type[i] == 0) return 0;
-            
-            int left_len = 0;
-            if (i - 1 == segL) left_len = max(0, end_idx[i - 1] - L + 1);
-            else left_len = end_idx[i - 1] - start[i - 1] + 1;
-            
-            int right_len = 0;
-            if (i + 1 == segR) right_len = max(0, R - start[i + 1] + 1);
-            else right_len = end_idx[i + 1] - start[i + 1] + 1;
-            
-            return left_len + right_len;
-        };
-        
-        vector<int> res;
-        
-        for (const auto& q : queries) {
-            int L = q[0];
-            int R = q[1];
-            
-            int segL = pos_to_seg[L];
-            int segR = pos_to_seg[R];
-            
-            if (segR - segL < 2) {
-                res.push_back(total_ones);
+
+        vector<int>ans;
+
+        int sz=pair_sum.size();
+        seg.assign(4*sz,0);
+
+        buildsegtree(0,0,sz-1,pair_sum);
+
+        for (auto q:queries){
+            int l=q[0];
+            int r=q[1];
+
+            int max_pairsum=0;
+            int low=lower_bound(endInd.begin(),endInd.end(),l)-endInd.begin();
+            int high=upper_bound(startInd.begin(),startInd.end(),r)-startInd.begin()-1;
+            if (low>=high) {
+                ans.push_back(total_one);
                 continue;
             }
+
+            int low_size=endInd[low]-max(l,startInd[low])+1;
+            int high_size=min(r,endInd[high])-startInd[high]+1;
             
-            int max_gain = 0;
-            max_gain = max(max_gain, eval(segL + 1, L, R, segL, segR));
-            max_gain = max(max_gain, eval(segR - 1, L, R, segL, segR));
-            
-            if (segL + 2 <= segR - 2) {
-                max_gain = max(max_gain, queryRMQ(segL + 2, segR - 2));
+            if (high-low==1){
+                max_pairsum=low_size+high_size;
             }
-            
-            res.push_back(total_ones + max_gain);
+            else if (low<high){
+                int pair1=low_size+size[low+1];
+                int pair2=high_size+size[high-1];
+                int res=Range_Maximum(0,low+1,high-2,0,sz-1,pair_sum);
+                max_pairsum=max({res,pair1,pair2});
+            }
+            ans.push_back(total_one+max_pairsum);
+
         }
-        
-        return res;
+        return ans;
     }
 };
