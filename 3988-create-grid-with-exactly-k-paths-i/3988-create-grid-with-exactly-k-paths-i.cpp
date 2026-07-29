f78d80aae0c3f80cbc5fb9bab58f293e8a60e8b8
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-07-29 16:05:56 +0530

    Time: 0 ms (100%), Space: 9.6 MB (57.8%) - LeetHub

--- 3988-create-grid-with-exactly-k-paths-i/3988-create-grid-with-exactly-k-paths-i.cpp
+++ 3988-create-grid-with-exactly-k-paths-i/3988-create-grid-with-exactly-k-paths-i.cpp
@@ -4,6 +4,7 @@ public:
 
         vector<string>grid (m,string(n,'#'));
 
+        // case 1
         if (k==1 && (n==1 || m==1)){
             for (int i=0;i<m;i++){
                 for (int j=0;j<n;j++){
@@ -13,12 +14,15 @@ public:
             return grid;
         }
 
+        // case 2
         if ((max(n,m)*(min(n,m)-1))<k) return {};
-        
+
+        // case 3
         if (n==3 && m==3 && k==4){
             return {"..#","...","#.."};
         }
 
+        // manage rest of the cases
         for (int i=0;i<n;i++){
             grid[0][i]='.';
         }
