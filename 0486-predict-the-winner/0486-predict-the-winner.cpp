91ff1a0dab2fafc77026a6ed2078c04254a62a7f
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-08-01 11:38:09 +0530

    Time: 22 ms (27.88%), Space: 9.7 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
+    int dp[21][21][2];
     int solve(int s,int e,int turn,vector<int>& nums){
         if (s>e) return 0;
-
+        if (dp[s][e][turn]!=-1) return dp[s][e][turn];
         int start=-1e7;
         int end=-1e7;
         if (turn==1){
@@ -14,11 +15,12 @@ public:
             start=-nums[s]+solve(s+1,e,1,nums);
             end=-nums[e]+solve(s,e-1,1,nums);
         }
-        return min(start,end);
+        return dp[s][e][turn]=min(start,end);
     }
     bool predictTheWinner(vector<int>& nums) {
-        int e=nums.size()-1;
-        int ans=solve(0,e,1,nums);
+        int e=nums.size();
+        memset(dp,-1,sizeof(dp));
+        int ans=solve(0,e-1,1,nums);
         if (ans>=0) return true;
         return false;
     }
