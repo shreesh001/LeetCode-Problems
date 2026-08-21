5ed3ff52a9034bc75fdfceb2d9cda3bdcdf96eb1
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-08-22 04:24:53 +0530

    Time: 168 ms (46.72%), Space: 20.7 MB (48.18%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -1,56 +1,39 @@
 class Solution {
 public:
-    typedef long long ll;
-
-    ll countSmaller(ll mid, vector<int>& coins) { 
-        ll correctedCount = 0;
-        int n = coins.size();
-
-        //2^n * n * log(maxCoin)
-        for(int expressions = 1; expressions <= (1 << n)-1; expressions++) { //2^n-1 expressions
-            ll lcm = 0;
-            ll order = 0; //even or odd order of expressions
-
-            for(int i = 0; i < n; i++) {
-                if(expressions & (1 << i)) {
-                    order++; //we have taken ith coin
-
-                    if(lcm == 0) {
-                        lcm = coins[i];
-                    } else {
-                        lcm = lcm * coins[i] / gcd(lcm, coins[i]);
+    using ll=long long;
+    long long count_number(ll mid,vector<int>& coins){
+        int n=coins.size();
+        ll count=0;
+        for (ll exp=1;exp<=(1<<n)-1;exp++){
+            ll order=0;
+            ll lcm=0;
+            for (ll i=0;i<n;i++){
+                if (exp&(1<<i)){
+                    order++;
+                    if (lcm==0) lcm=coins[i];
+                    else{
+                        lcm=(lcm*coins[i])/gcd(lcm,coins[i]);
                     }
                 }
             }
-
-            if(order % 2 == 0) { //even then subtract
-                correctedCount -= mid/lcm;
-            } else {
-                correctedCount += mid/lcm;
-            }
+            if (order&1) count+=(mid/lcm);
+            else count-=(mid/lcm);
         }
-
-        return correctedCount;
+        return count;
     }
+    long long findKthSmallest(vector<int>& coins, int k) {
+        ll res=-1;
+        ll low=1;
+        ll high=k*1ll*(*max_element(coins.begin(),coins.end()));
 
-    ll findKthSmallest(vector<int>& coins, int k) {
-        ll result = -1;
-
-        ll l = 1;
-        ll r = (ll)(*max_element(begin(coins), end(coins))) * k;
-
-        //log(maxCoin * k) * 2^n * n * log(maxCoin)
-        while(l <= r) {
-            ll mid = l + (r-l)/2;
-
-            if(countSmaller(mid, coins) >= k) { //left side me we can find kth smallest element
-                result = mid;
-                r = mid-1;
-            } else {
-                l = mid+1;
+        while(low<=high){
+            ll mid=(low+high)/2;
+            if (count_number(mid,coins)>=k){
+                res=mid;
+                high=mid-1;
             }
+            else low=mid+1;
         }
-
-        return result;
+        return res;
     }
 };
