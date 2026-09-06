5790655f2b0e5217dbe4031f60db2fda5ce5f68b
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-09-06 17:50:45 +0530

    Time: 11 ms (50.01%), Space: 31.1 MB (18.32%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -3,33 +3,27 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<vector<int>>pref(m,vector<int>(n,0));
+        
+        vector<vector<int>>dp(m,vector<int>(n,0));
+
+        int side=0;
         for (int i=0;i<m;i++){
-            for (int j=0;j<n;j++){
-                pref[i][j]+=(matrix[i][j]-'0');
-                if (j-1>=0) pref[i][j]+=pref[i][j-1];
-                if (i-1>=0) pref[i][j]+=pref[i-1][j];
-                if (i-1>=0 && j-1>=0) pref[i][j]-=pref[i-1][j-1];
-            }
+            dp[i][0]=matrix[i][0]-'0';
+            side=max(side,dp[i][0]);
+        }
+        for (int i=0;i<n;i++){
+            dp[0][i]=matrix[0][i]-'0';
+            side=max(side,dp[0][i]);
         }
 
-        int maxarea=0;
-        for (int i=0;i<m;i++){
-            for (int j=0;j<n;j++){
-                if (matrix[i][j]=='0') continue;
-                for (int k=0;k<min(m,n);k++){
-                    if (i+k>=m || j+k>=n) break;
-                    int area=pref[i+k][j+k];
-                    if (j-1>=0) area-=pref[i+k][j-1];
-                    if (i-1>=0) area-=pref[i-1][j+k];
-                    if (j-1>=0 && i-1>=0) area+=pref[i-1][j-1];
-                    if (area==((k+1)*(k+1))){
-                        maxarea=max(area,maxarea);
-                    }
-                    else break;
+        for (int i=1;i<m;i++){
+            for (int j=1;j<n;j++){
+                if (matrix[i][j]=='1'){
+                    dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
                 }
+                side=max(side,dp[i][j]);
             }
         }
-        return maxarea;
+        return (side*side);
     }
 };
