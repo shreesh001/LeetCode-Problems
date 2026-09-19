25ce37e2c9184afb006012aa42c0994d3b3af550
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-09-19 11:50:24 +0530

    Time: 5 ms (88.77%), Space: 29.4 MB (24.78%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -1,54 +1,56 @@
 class Solution {
 public:
-    struct Seg {
-        int left, right;
-        bool operator<(const Seg& rhs) const {
-            if (right == rhs.right) {
-                return left > rhs.left;
-            }
-            return right < rhs.right;
-        }
-    };
-
     vector<string> maxNumOfSubstrings(string s) {
-        vector<Seg> seg(26, (Seg){-1, -1});
-        // Preprocess the left and right endpoints.
-        for (int i = 0; i < s.length(); ++i) {
-            int charIdx = s[i] - 'a';
-            if (seg[charIdx].left == -1) {
-                seg[charIdx].left = seg[charIdx].right = i;
-            } else {
-                seg[charIdx].right = i;
+        int n=s.size();
+
+        vector<int>first(26,-1),last(26,-1);
+        vector<int>valid(n,false);
+        for (int i=0;i<n;i++){
+            if (first[s[i]-'a']==-1){
+                first[s[i]-'a']=i;
+                valid[i]=true;
             }
+            last[s[i]-'a']=i;
         }
-        for (int i = 0; i < 26; ++i) {
-            if (seg[i].left != -1) {
-                for (int j = seg[i].left; j <= seg[i].right; ++j) {
-                    int charIdx = s[j] - 'a';
-                    if (seg[i].left <= seg[charIdx].left &&
-                        seg[charIdx].right <= seg[i].right) {
-                        continue;
-                    }
-                    seg[i].left = min(seg[i].left, seg[charIdx].left);
-                    seg[i].right = max(seg[i].right, seg[charIdx].right);
-                    j = seg[i].left;
+
+        
+
+
+        for (int c=0;c<26;c++){
+            int start=first[c];
+            int end=last[c];
+
+            if (start==-1) continue;
+
+            for (int i=start;i<=end;i++){
+                int ch=s[i];
+                if (first[ch-'a']<start){
+                    valid[start]=false;
+                    break;
                 }
+                end=max(last[ch-'a'],end);
+                last[c]=end;
             }
         }
-        // Greedily select intervals.
-        sort(seg.begin(), seg.end());
-        vector<string> ans;
-        int end = -1;
-        for (auto& segment : seg) {
-            int left = segment.left, right = segment.right;
-            if (left == -1) {
-                continue;
-            }
-            if (end == -1 || left > end) {
-                end = right;
-                ans.emplace_back(s.substr(left, right - left + 1));
+
+        vector<string>res;
+
+        int lastind=n+5;
+
+        for (int i=n-1;i>=0;i--){
+            int ch=s[i];
+            
+            if (!valid[i]) continue;
+
+            int start=first[ch-'a'];
+            int end=last[ch-'a'];
+
+            if (end<lastind){
+                res.push_back(s.substr(start,end-start+1));
+                lastind=start;
             }
         }
-        return ans;
+
+        return res;
     }
 };
