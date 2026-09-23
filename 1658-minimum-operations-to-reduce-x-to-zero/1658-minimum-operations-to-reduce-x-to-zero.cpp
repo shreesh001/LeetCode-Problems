4e7b8891460f3b0f60d419de9d108f084c477620
Shreesh Pathak <163734380+shreesh001@users.noreply.github.com>
2026-09-23 12:20:08 +0530

    Time: 215 ms (11.4%), Space: 172.3 MB (7.59%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
-        unordered_map<int,int>mpp_pref,mpp_suff;
-
+        unordered_map<int,int>mpp_suff;
+        mpp_suff[0]=0;
         int ans=n+1;
-        int pref=0,suff=0;
+        int suff=0;
         for (int i=0;i<n;i++){
-            pref+=nums[i];
             suff+=nums[n-1-i];
 
-            if (pref==x){
-                ans=min(ans,i+1);
-            }
             if (suff==x){
                 ans=min(ans,i+1);
             }
 
-            mpp_pref[pref]=i+1;
             mpp_suff[suff]=i+1;
         }
 
-        pref=0;
+        int pref=0;
         for (int i=0;i<n;i++){
             pref+=nums[i];
             if (pref>x) break;
